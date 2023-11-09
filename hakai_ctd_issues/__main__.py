5292f2a464d0d228f989284a1825e74cda58c66f
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-11-09 09:55:40 -0500

    generate summary page per orgnization with Jinja

--- hakai_ctd_issues/__main__.py
+++ hakai_ctd_issues/__main__.py
@@ -1,11 +1,17 @@
 import json
+import re
 from pathlib import Path
+
 import pandas as pd
-from hakai_api import Client
 import plotly.express as px
+from hakai_api import Client
+from jinja2 import Environment, FileSystemLoader
+
+environment = Environment(loader=FileSystemLoader("hakai_ctd_issues/templates/"))
 
 
 client = Client()
+site = Path("site")
 CTD_CASTS_FIELDS = [
     "organization",
     "work_area",
@@ -25,7 +31,7 @@ assignees:
 ---
 
 ## issue
-The Hakai CTD Processing tool encountered the following problem which is affecting {N_hakai_id} hakai_ids:
+The Hakai CTD Processing tool encountered the following problem which is affecting {count} hakai_ids:
 {process_error_message}
 
 !!! notes
@@ -52,9 +58,35 @@ def get_errors():
         + f"process_error!=null&process_error!=''&limit=-1&fields={','.join(CTD_CASTS_FIELDS)}"
     )
     response.raise_for_status()
-    df = pd.DataFrame(response.json())
-    df["process_error_message"] = df["process_error"].apply(_get_error_message)
-    return df
+    errors = pd.DataFrame(response.json())
+    errors["process_error_message"] = errors["process_error"].apply(_get_error_message)
+    return errors
+
+
+def get_summarized_errors(errors):
+    def _get_subset(items, max_items=4):
+        if isinstance(items, (list, pd.Series)):
+            items = list(items)
+            if len(items) > max_items:
+                items = items[:max_items]
+                items += ["..."]
+            return items
+        return items
+
+    summarized_errors = errors.groupby(
+        ["organization", "work_area", "process_error_message"]
+    ).agg(
+        {
+            "hakai_id": [_get_subset, "count"],
+            "process_error": "first",
+            "station": list,
+        }
+    )
+    summarized_errors.columns = ["hakai_ids", "count", "process_error", "stations"]
+    summarized_errors = summarized_errors.sort_values(
+        ["organization", "count"], ascending=False
+    )
+    return summarized_errors.reset_index()
 
 
 def main(output="ctd-issues.html"):
@@ -63,40 +95,60 @@ def main(output="ctd-issues.html"):
             items = list(items)
             if len(items) > max_items:
                 items = items[:max_items]
-                items += ['...']
+                items += ["..."]
             return items
         return items
-    
+
     def _get_issue_md(issue):
         return ISSUE_TEMPLATE.format(**issue.to_dict())
 
     errors = get_errors()
+    summarized_errors = get_summarized_errors(errors)
+    summarized_errors["process_error_message_short"] = summarized_errors[
+        "process_error_message"
+    ].apply(lambda x: re.sub('[\{"\]', "", x.split(".")[0]) if x else x)
+    summarized_errors["issues_md"] = summarized_errors.apply(_get_issue_md, axis=1)
 
-    # Group errors
-    summarized_errors = errors.groupby(["organization", "work_area", "process_error_message"]).agg(
-        {
-            "hakai_id": [_get_subset, "count"],
-            "process_error": "first",
-            "station": list,
-        }
-    )
-    summarized_errors.columns = ["hakai_ids", "N_hakai_id", "process_error", "stations"]
-    summarized_errors = summarized_errors.sort_values(
-        ["organization", "N_hakai_id"], ascending=False
-    )
-    summarized_errors = summarized_errors.reset_index()
-    summarized_errors['issues_md'] = summarized_errors.apply(_get_issue_md,axis=1)
     # Load existing github issues
 
-    # Drop already existing ones
+    # Compare already existing ones
+
+    # Close resolved issues
+
+    # Update existing issues
 
     # Generate github new issues
-    fig = px.sunburst(summarized_errors,path=["organization",'work_area','process_error_message'], color='N_hakai_id')
-    Path("sumburs.html").write_text(fig.to_html())
-    summarized_errors.to_html(output)
+    if not site.exists():
+        site.mkdir(parents=True, exist_ok=True)
     for id, issue in summarized_errors.iterrows():
-        (Path("issues")/ f"issue-{id}.md").write_text(issue['issues_md'])
+        (Path("issues") / f"issue-{id}.md").write_text(issue["issues_md"])
 
+    # Generate summary page per organization
+    for organization, df_org in summarized_errors.groupby("organization"):
+        organization = organization.replace(" ", "_")
+        org_dir = site / organization
+        org_dir.mkdir(parents=True, exist_ok=True)
+
+        sunburst_figure_html = px.sunburst(
+            df_org,
+            path=["work_area", "process_error_message_short"],
+            values="count",
+            color="count",
+        ).to_html(full_html=False, include_plotlyjs="cdn")
+
+        summary_table_html = df_org[
+            ["work_area", "process_error_message", "count"]
+        ].to_html(
+            index=False, classes=["table-bordered", "table-striped", "table-hover"]
+        )
+
+        organization_summary = environment.get_template("issue_summary.html")
+        summary_page = organization_summary.render(
+            organization=organization,
+            sunburst_figure_html=sunburst_figure_html,
+            summary_table_html=summary_table_html,
+        )
+        (org_dir / "index.html").write_text(summary_page)
 
 
 if __name__ == "__main__":
